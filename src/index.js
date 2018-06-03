6259f2500bf53c15b4510fdbb6c84f962c616c27
Harry Vo <huyvoxuan8489@gmail.com>
2018-06-03 21:18:22 +0800

    remmove css in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap';
-import style from './index.css';
 import './index.scss';
 import AppContainer from './AppContainer';
 import ReduxContainer from './ReduxContainer';
