f7579625466fc4bd6768bd2b8561538e5ff4f7c9
Harry Vo <huyvoxuan8489@gmail.com>
2018-06-16 11:16:44 +0800

    reduce component

--- src/app/components/OverLay/OverLay.js
+++ src/app/components/OverLay/OverLay.js
@@ -1,25 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
 import './OverLay.scss';
 import spinnerImg from '../../../assets/images/logo.svg';
-
-class OverLay extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div className={`overlay ${ this.props.isLoading ? 'active' : '' }`}>
-                <img src={spinnerImg} className={`img-overlay animation`} alt="logo" />
-            </div>
-        )
-    }
-}
-OverLay.defaultProps = {
-    isLoading: false
-}
-OverLay.propTypes = {
-    isLoading: PropTypes.bool
-}
-export default connect()(OverLay);
+const OverLay = ({isLoading}) => (
+    <div className={`overlay ${ isLoading ? 'active' : '' }`}>
+        <img src={spinnerImg} className={`img-overlay animation`} alt="logo" />
+    </div>
+);
+export default OverLay;
