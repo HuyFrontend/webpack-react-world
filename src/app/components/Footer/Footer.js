10e0b11b65a7f032cbc9199286d3775de80dbb7c
Harry Vo <huyvoxuan8489@gmail.com>
2018-06-16 11:20:19 +0800

    reduce footer component

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Footer.scss';
 
-class Footer extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <footer className="Footer">
-                <div className="container">
-                    <div className="page-footer font-small blue pt-4 mt-4">
-                        <div>
-                            <h5 className="text-uppercase">Angular 6 WEPACK</h5>
-                            <p>Here you can use rows and columns here to organize your footer content.</p>
-                        </div>
-                        <div className="footer-copyright text-center py-3">© 2018 Copyright:
-                            <a href="https://google.com"> Google.com</a>
-                        </div>
-                    </div>
+const Footer = () => (
+    <footer className="Footer">
+        <div className="container">
+            <div className="page-footer font-small blue pt-4 mt-4">
+                <div>
+                    <h5 className="text-uppercase">Angular 6 WEPACK</h5>
+                    <p>Here you can use rows and columns here to organize your footer content.</p>
                 </div>
-            </footer>
-        )
-    }
-}
+                <div className="footer-copyright text-center py-3">© 2018 Copyright:
+                    <a href="https://google.com"> Google.com</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+);
 export default Footer;
